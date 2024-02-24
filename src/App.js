a5d08a95c882364e8d9fb0338750cab1e9b7e02e
mrnayak25 <mrnayak27@gmail.com>
2024-02-24 17:19:45 +0530

    haaaa

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import logo from "./Assets/img/logo.png";
-import control from "./Assets/icons/control.png";
 import homeic from "./Assets/icons/home.png";
 import gamesic from "./Assets/icons/games.png";
 import newsic from "./Assets/icons/news.png";
@@ -26,14 +25,14 @@ function App() {
 
   return (
     <div className="flex bg-black justify-center items-center h-screen">
-      <div className={`bg-[#222222] me-2 relative duration-300 m-10 items-center rounded-lg grid justify-items-center`}>
+      <div className={`bg-[#222222] me-2 relative duration-300 p-2 m-2 items-center rounded-lg grid justify-items-center`}>
         {/* <img
           src={control}
           className={`absolute cursor-pointer -right-4 top-9 w-7 border-dark-purple
            border-2 rounded-full  ${!open && "rotate-180"}`}
           onClick={() => setOpen(!open)} alt='img'
         /> */}
-        <div className="flex gap-x-4 pt-2 items-center">
+        <div className="flex gap-x-4 mt-4 items-center">
           <img src={logo} className={`cursor-pointer duration-500 h-20`} alt='img' />
           {/* <h1
             className={`text-white origin-left font-medium text-xl duration-200 ${
@@ -42,15 +41,15 @@ function App() {
           >SEN10
           </h1> */}
         </div>
-        <div className="ps-1 flex-col justify-between">
+        <div className="ps-1 flex-col justify-center gap-3 text-center ">
 
           {Menus.map((Menu, index) => (
-            <div className='bg-[#302f2f] rounded-m w-3/4 text-center ' key={index}>
+             
+            <div className='bg-[#302f2f] rounded-lg w-[90%] text-center flex justify-center my-2' key={index}>
               <div
-                className={` rounded-md text-center place-content-center cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 p-2 
-              ${Menu.gap ? "mt-9" : "mt-2"} ${index === 0 && "bg-light-white"
-                  } `}
-              >
+                className={`flex-col justify-center  rounded-md text-center place-content-center cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 p-2 
+              ${Menu.gap ? "mt-9" : "mt-2"} ${index === 0 && "bg-light-white" } `}>
+               
                 <img src={Menu.src} alt='img' className='text-center place-content-center' />
                 <span className={`${!open && "hidden"} origin-left duration-200`}>
                   {Menu.title}
@@ -58,9 +57,7 @@ function App() {
               </div>
             </div>
           ))}
-
         </div>
-
       </div>
       <div className="h-screen flex-1 p-7">
       <div className="flex-1  ">
@@ -71,10 +68,10 @@ function App() {
               <p className="text-3x font-bold text-white text-center">Search</p>
             </div>
             <div className="flex items-center">
-              <a href="#" className="text-gray-400 hover:text-gray-200 mr-4 underline-none">
+              <a href="/" className="text-gray-400 hover:text-gray-200 mr-4 underline-none">
                 WishList
               </a>
-              <a href="#" className=" text-gray-400 hover:text-gray-200 underline-none">
+              <a href="/" className=" text-gray-400 hover:text-gray-200 underline-none">
                 <div className='flex items-center bg-[#222222] p-2 m-2 rounded-sm'>
                   <p className="text-white mr-1">Cart</p>
                   <p className='bg-[#ffffff] text-black p-1 rounded-sm'>9</p>
@@ -84,7 +81,6 @@ function App() {
           </header>
         </main>
       </div>
-      
       <MainContainer/>
     </div>
     </div>
