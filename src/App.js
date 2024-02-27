4ab5104fa75a1d2bc7a6e20820845ca4635c49fb
mrnayak25 <mrnayak27@gmail.com>
2024-02-27 07:18:26 +0530

    routes fixed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import { Route, Routes } from 'react-router-dom';
-
+import { Route, Routes } from "react-router-dom";
+import News from "./components/News";
+import Games from "./components/Games";
+import Esports from "./components/Esports";
+import About from "./components/About";
+import Library from "./components/Library";
+import Home from "./components/Home";
 import React, { useState } from "react";
-import NavBar from './components/NavBar';
-import SignIn from './components/SignIn';
-
+import NavBar from "./components/NavBar";
+import SignIn from "./components/SignIn";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route exact path='/' element={<NavBar/>}/>
-      <Route exact path='/signin' element={<SignIn/>}/>
-    </Routes>
+      <Routes>
+        <Route exact path="/" element={<NavBar />}>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/games" element={<Games />} />
+          <Route exact path="/news" element={<News />} />
+          <Route exact path="/esports" element={<Esports />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/library" element={<Library />} />
+        </Route>
+        <Route exact path="/signin" element={<SignIn />} />
+      </Routes>
     </>
   );
-};
+}
 
 export default App;
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,24 +1,10 @@
 import React from 'react'
-import SliderComp from './SliderComp'
-import Home from './Home'
-import { Route, Routes } from 'react-router-dom'
-import News from './News'
-import Games from './Games'
-import Esports from './Esports'
-import About from './About'
-import Library from './Library'
+import { Outlet, Route, Routes } from 'react-router-dom'
 
 function MainContainer() {
   return (
     <div>
-      <Routes>
-        <Route exact path='/' element={ <Home/>}/>
-        <Route exact path='/games' element={<Games/>}/>
-        <Route exact path='/news' element={<News/>}/>
-        <Route exact path='/esports'element={<Esports/>}/>
-        <Route exact path='/about'element={<About/>}/>
-        <Route exact path='/library'element={<Library/>}/>
-        </Routes>
+      <Outlet/>
     </div>
   )
 }
